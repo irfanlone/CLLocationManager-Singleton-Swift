53eb5f197a1ebb1a5aa309f37d358b270138b842
Irfan <irfan.mf69@gmail.com>
2016-03-30 11:34:23 -0700

    Location Manager Singleton Example

---
+++ Example/LocationManagerSingleton/UserLocationManager.swift
@@ -0,0 +1,62 @@
+//
+//  Copyright © 2016 Irfan Lone.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+
+import MapKit
+
+protocol LocationUpdateProtocol {
+    func locationDidUpdateToLocation(location : CLLocation)
+}
+
+
+class UserLocationManager: NSObject, CLLocationManagerDelegate {
+    
+    static let SharedManager = UserLocationManager()
+    
+    private var locationManager = CLLocationManager()
+    
+    var currentLocation : CLLocation?
+    
+    var delegate : LocationUpdateProtocol!
+    
+    private override init () {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLLocationAccuracyHundredMeters
+        locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if currentLocation == nil {
+            currentLocation = locationManager.location
+        }
+        currentLocation = newLocation;
+        self.delegate.locationDidUpdateToLocation(currentLocation!)
+    }
+    
+}
+
+
+
